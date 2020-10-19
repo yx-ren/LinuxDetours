bdeb4b88daa762b15e9b9baec7ffbca043cd2fe8
yx-ren <yunxiang.r@gmail.com>
2020-10-19 14:51:03 +0800

    update unit test shell
    Conflicts:
    	LinuxDetours/Makefile

---
+++ LinuxDetours/run_test.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+DUMP_SH="./dump_obj.sh"
+if [ ! -f $DUMP_SH ];then
+    echo "file:[$DUMP_SH] not existed"
+    exit -1
+fi
+
+OBJS=("linux_detours.test" "libdetours64.so")
+
+for ((i = 0; i != ${#OBJS[@]}; i++))
+do
+    obj=${OBJS[$i]}
+    $DUMP_SH $obj
+done
+
+UNIT_TEST="./linux_detours.test"
+UNIT_TEST_RESULT_FILE="unit_test_result"
+$UNIT_TEST > $UNIT_TEST_RESULT_FILE 2>&1
