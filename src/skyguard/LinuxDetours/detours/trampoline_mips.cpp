b2371715f027c6ead0627ee7c85828ac5d81c993
yx-ren <yunxiang.r@gmail.com>
2020-11-17 19:05:13 +0800

    review asm code

--- src/skyguard/LinuxDetours/detours/trampoline_mips.cpp
+++ src/skyguard/LinuxDetours/detours/trampoline_mips.cpp
@@ -59,8 +59,7 @@ trampoline_template_mips64:
         bal 4
         move $14,$31            # move t2,ra
         move $31,$15            # move ra,t3
-        li $12, 12              # move t0,12    
-        dsubu $13, $14, $12     # t1 = t2 + t0 // $t1 save the base line
+        daddiu $13, $14, -12    # t1 = t2 - 12 // $t1 save the base line, 12 is offset from base line to PC
 
         # -------------------- #
 
@@ -73,13 +72,7 @@ trampoline_template_mips64:
         sd $a3, (4 * 8)($sp)    # protected $a3 register (Arguments to functions)
         sd $a4, (5 * 8)($sp)    # protected $a4 register (Arguments to functions)
         sd $t1, (6 * 8)($sp)    # save the base line(_DETOUR_TRAMPOLINE.rbTrampolineCode)
-        sd $t9, (7 * 8)($sp)    # save the base line(_DETOUR_TRAMPOLINE.rbTrampolineCode)
-
-        # -------------------- #
-
-        # test segment #
-#j call_hook_handler
-        # test segment #
+        sd $t9, (7 * 8)($sp)    # save the $t9 register
 
 ###################################################################################### call hook handler or original method...
 ## call NET intro
@@ -91,34 +84,28 @@ trampoline_template_mips64:
 #    call qword ptr [%rip + NETIntro] ## Hook->NETIntro(Hook, RetAddr, InitialRSP)##
 
 ## should call original method?
-## TODO......
     # get arg1
-    ld $a0, (6 * 8)($sp)    # restore base line to $a0
+    ld $a0, (6 * 8)($sp)    # a0 save restore base line
 
     # get arg2
     move $a1, $ra           # a1 save the return address
 
     # get arg3
-    daddiu  $a2, $sp, 128   # a2 save the origin rsp
+    daddiu $a2, $sp, 128    # a2 save the origin rsp
 
     # get [NETIntro] address
-    ld $13, (6 * 8)($sp)    # restore base line to t1 
-    li $12, 40              # li t0,40 //40 is [NETIntro] offset from base line
-    dsubu $13, $13, $12     # dsubu t1,t1,t0 // $t1 save the address of the [NETIntro] address
+    ld $13, (6 * 8)($sp)    # restore base line to $t1 
+    daddiu $13, $13, -40    # t1 -= 40 // $t1 save the address of the [NETIntro] address
     ld $12, 0($13)          # ld t0, t1 // read 8 byte from $t1, t0 save the [NETIntro] address
 
-    # call [NETIntro]
+    # call Hook->NETIntro(Hook, RetAddr, InitialRSP)
     move $t9, $12
     jalr $t9
-    bne $v0, 0, call_hook_handler
-
-#bne $a1, $a1, call_hook_handler
+    bne $v0, 0, call_hook_handler # if BarrierIntro() return true, call hook proc
 
     ## call original method
-#li $13, 0xffffffff
     ld $13, (6 * 8)($sp)    # restore base line to t1 
-    li $12, 32              # li t0,32 //32 is [OldProc] offset from base line
-    dsubu $13, $13, $12     # dsubu t1,t1,t0 // $t1 save the address of the [OldProc] address
+    daddiu $13, $13, -32    # t1 -= 32 // $t1 save the address of the [OldProc] address
     ld $12, 0($13)          # ld t0, t1 // read 8 byte from $t1, t0 save the [OldProc] address
 #move $25, $12              # in this case, t9 can't be modifed (jr $register is offset 8 byte after t9)
                             # so use anther register saved address and used to jump
@@ -141,10 +128,8 @@ trampoline_template_mips64:
 
 call_hook_handler:
     # get Hook Proc Address and saved into $t9 register
-#li $13, 0xffffffff
     ld $13, (6 * 8)($sp)    # restore base line to t1 
-    li $12, 24              # li t0,24 //24 is [NewProc] offset from base line
-    dsubu $13, $13, $12     # dsubu t1,t1,t0 // $t1 save the address of the [NewProc] address
+    daddiu $13, $13, -24    # t1 -= 24 // $t1 save the address of the [NewProc] address
     ld $12, 0($13)          # ld t0, t1 // read 8 byte from $t1, t0 save the [NewProc] address
     move $25, $12           # save [NewProc] to $t9, because [$gp] calculate with $t9 in next stack
 
@@ -167,8 +152,8 @@ trampoline_exit:
 
         # -------------------- #
 
-        # jump to the address(address saved in $t9, [OldProc] or [HookProc])
-        move $25, $12           # save [NewProc] to $t9, because [$gp] calculate with $t9 in next stack
+        # jump to the address
+        move $25, $12           # move $t9, $t0
         jr $25                  # jr $t9
 
 ######################################################################################
